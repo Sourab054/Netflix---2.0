e165c73281c2124ad6b7362236adbf83d90755ee
Sourab Patil <sourab054@gmail.com>
2022-09-13 14:35:19 +0530

    Logging functionality

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -33,7 +33,7 @@ function Plans({ products }: Props) {
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className="border-b border-white/10 bg-[#141414]">
+      <header className="border-b border-white/10 bg-[#010511] z-10">
         <Link href="/">
           <img
             src="https://rb.gy/ulxxee"
